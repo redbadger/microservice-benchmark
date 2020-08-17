2c9b8b28e7416db81f19a440f097baa15e8535de
Stuart Harris <stuart.harris@red-badger.com>
2020-08-17 14:12:35 +0100

    randomise delay (1-3s) and return json

--- legacy-backend-rust/src/main.rs
+++ legacy-backend-rust/src/main.rs
@@ -1,16 +1,36 @@
-use async_std::{future, prelude::*};
-use std::time::Duration;
-use tide::Request;
+use async_std::{sync::Mutex, task};
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+use std::{sync::Arc, time::Duration};
+use tide::{http::mime, Request, Response};
 
-async fn handle_request(_req: Request<()>) -> tide::Result<String> {
-    let delay = future::ready(1).delay(Duration::from_millis(2000));
-    delay.await;
-    Ok("Hello, world!".to_string())
+#[derive(Clone)]
+struct State {
+    data: String,
+    rng: Arc<Mutex<SmallRng>>,
+}
+
+async fn handle_request(req: Request<State>) -> tide::Result {
+    let state = req.state();
+
+    let delay = {
+        let arc = state.rng.clone();
+        let mut rng = arc.lock().await;
+        rng.gen_range(1000, 3000)
+    };
+    task::sleep(Duration::from_millis(delay)).await;
+
+    let res = Response::builder(200)
+        .body(state.data.to_string())
+        .content_type(mime::JSON)
+        .build();
+    Ok(res)
 }
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
     // tide::log::start();
-    let mut app = tide::new();
+    let data = include_str!("../../data/generated-1.json").to_string();
+    let rng = Arc::new(Mutex::new(SmallRng::from_entropy()));
+    let mut app = tide::with_state(State { data, rng });
     app.at("/").get(handle_request);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
