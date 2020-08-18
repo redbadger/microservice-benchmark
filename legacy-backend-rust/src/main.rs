7f162145852e03e7464606d1d446f0dc403a064c
Stuart Harris <stuart.harris@red-badger.com>
2020-08-18 11:07:14 +0100

    return different json (1-10 random)

--- legacy-backend-rust/src/main.rs
+++ legacy-backend-rust/src/main.rs
@@ -1,37 +1,46 @@
+use anyhow::Result;
 use async_std::{
     sync::{Arc, Mutex},
     task,
 };
 use rand::{rngs::SmallRng, Rng, SeedableRng};
+use serde_json::Value;
 use std::time::Duration;
 use tide::{http::mime, Request, Response};
-
 #[derive(Clone)]
 struct State {
-    data: String,
+    data: Vec<Value>,
     rng: Arc<Mutex<SmallRng>>,
 }
 
 async fn handle_request(req: Request<State>) -> tide::Result {
     let state = req.state();
 
-    let delay = {
+    let (delay, index) = {
         let arc = state.rng.clone();
         let mut rng = arc.lock().await;
-        rng.gen_range(1000, 3000)
+        (
+            rng.gen_range(1000, 3000),
+            rng.gen_range(0, state.data.len() - 1),
+        )
     };
     task::sleep(Duration::from_millis(delay)).await;
 
     let res = Response::builder(200)
-        .body(state.data.to_string())
+        .body(state.data[index].to_string())
         .content_type(mime::JSON)
         .build();
     Ok(res)
 }
 #[async_std::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<()> {
     // tide::log::start();
-    let data = include_str!("../data/generated-1.json").to_string();
+    let data = include_str!("../data/generated-1.json");
+    let data: Value = serde_json::from_str(data)?;
+    let data = data
+        .as_array()
+        .ok_or_else(|| anyhow::anyhow!("data is not an array"))?
+        .to_vec();
     let rng = Arc::new(Mutex::new(SmallRng::from_entropy()));
     let mut app = tide::with_state(State { data, rng });
     app.at("/").get(handle_request);
