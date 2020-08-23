b3a1b8285f160131c7ecf09c56ede308477c0d4a
Stuart Harris <stuart.harris@red-badger.com>
2020-08-23 15:02:57 +0100

    add structopt so we can pass env vars

--- api-rust/src/main.rs
+++ api-rust/src/main.rs
@@ -1,60 +1,45 @@
-use async_std::task;
-use isahc::{config::VersionNegotiation, prelude::*};
-use std::{sync::Arc, time::Duration};
-use tide::{convert::DeserializeOwned, http::mime, log, Body, Request, Response, Result};
+use isahc::http::uri::Uri;
+use structopt::StructOpt;
+use tide::log;
+use tide::Result;
 
-mod model;
-
-#[derive(Clone)]
-struct State {
-    client: Arc<HttpClient>,
-}
-
-async fn get<T>(url: &str, client: Arc<HttpClient>) -> Result<T>
-where
-    T: DeserializeOwned,
-{
-    let x: T = client.get_async(url).await?.json()?;
-    Ok(x)
-}
-
-async fn handle_request(req: Request<State>) -> Result {
-    let state = req.state();
-
-    let accounts = task::spawn(get::<Vec<model::Account>>(
-        "http://localhost:3000/accounts",
-        state.client.clone(),
-    ));
-    let cards = task::spawn(get::<Vec<model::Card>>(
-        "http://localhost:3000/cards",
-        state.client.clone(),
-    ));
-    let customer = task::spawn(get::<model::Customer>(
-        "http://localhost:3000/customer",
-        state.client.clone(),
-    ));
-    let mut customer = customer.await?;
-    customer.accounts = Some(accounts.await?);
-    customer.cards = Some(cards.await?);
-
-    let mut res = Response::new(200);
-    res.set_content_type(mime::JSON);
-    res.set_body(Body::from_json(&customer)?);
-    Ok(res)
+#[derive(Debug, Clone, StructOpt)]
+pub struct Config {
+    #[structopt(long, env = "PORT", default_value = "8000")]
+    pub port: u32,
+    #[structopt(
+        long,
+        env = "URL_ACCOUNTS",
+        parse(try_from_str = str::parse),
+        default_value = "http://localhost:3000/accounts"
+    )]
+    url_accounts: Uri,
+    #[structopt(
+        long,
+        env = "URL_CARDS",
+        parse(try_from_str = str::parse),
+        default_value = "http://localhost:3000/cards"
+    )]
+    url_cards: Uri,
+    #[structopt(
+        long,
+        env = "URL_CUSTOMER",
+        parse(try_from_str = str::parse),
+        default_value = "http://localhost:3000/customer"
+    )]
+    url_customer: Uri,
 }
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    tide::log::with_level(log::LevelFilter::Error);
-    let client = Arc::new(
-        HttpClient::builder()
-            .version_negotiation(VersionNegotiation::http11())
-            .timeout(Duration::from_secs(20))
-            .build()?,
-    );
-    let mut app = tide::with_state(State { client });
-    app.at("/").get(handle_request);
-    app.listen("0.0.0.0:8000").await?;
+    let config = Config::from_args();
 
-    Ok(())
+    tide::log::with_level(log::LevelFilter::Error);
+
+    api_rust::create_app(config.url_accounts, config.url_cards, config.url_customer)
+        .await?
+        .listen(format!("0.0.0.0:{}", config.port))
+        .await?;
+
+    unreachable!()
 }
